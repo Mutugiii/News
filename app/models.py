4c3222dee73dae95607bbde2ac0335eb63a20c9d
Mutugi Mutuma <mutugimutuma@gmail.com>
2020-02-03 01:21:31 +0300

    Add Source and Article classes

--- app/models.py
+++ app/models.py
@@ -3,15 +3,26 @@ class Source:
     Source class to define source objects
     '''
 
-    all_sources = []
-
-    def __init__(self,id,name,description,url,category,language,country)
+    def __init__(self,id,name,description,url,category,language,country):
+        id = self.id
+        name = self.name
+        description = self.description
+        url = self.url
+        category = self.category
+        language = self.language
+        country = self.country 
 
 class Article:
     '''
     Article class to define article objectsa
 
     '''
-    all_articles = []
 
-    def __init__(self,author,title,description,url,urlToImage,publishedAt,content)
+    def __init__(self,author,title,description,url,urlToImage,publishedAt,content):
+        author = self.author
+        title = self.title
+        description = self.description
+        url = self.url
+        urlToImage = self.urlToImage
+        publishedAt = self.publishedAt
+        content = self.content
