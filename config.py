34bcc7ffe890a847912c1113bf57d4ebce822a6a
Mutugi Mutuma <mutugimutuma@gmail.com>
2020-02-03 14:59:30 +0300

    Add base url for search

--- config.py
+++ config.py
@@ -5,6 +5,7 @@ class Config:
     General configuration parent class
     '''
     ARTICLE_BASE_URL = 'https://newsapi.org/v2/top-headlines?q={}&apiKey={}'
+    SEARCH_BASE_URL = 'https://newsapi.org/v2/everything?q={}&apiKey={}'
     SOURCE_BASE_URL ='https://newsapi.org/v2/sources?apiKey={}'
     NEWS_API_KEY = os.environ.get('NEWS_API_KEY')
     SECRET_KEY = os.environ.get('SECRET_KEY')
