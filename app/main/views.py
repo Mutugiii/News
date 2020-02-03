c2c0a4f4447d3e656c6cf2b4208570f648777c10
Mutugi Mutuma <mutugimutuma@gmail.com>
2020-02-03 15:00:04 +0300

    Add function to search by keywords

--- app/main/views.py
+++ app/main/views.py
@@ -1,6 +1,6 @@
-from flask import render_template
+from flask import render_template,request,redirect,url_for
 from . import main
-from ..requests import get_articles
+from ..requests import get_articles,search_articles
 from ..models import Article
 
 @main.route('/')
@@ -10,8 +10,20 @@ def index():
     '''
 
     # Getting topics
-    technology_articles = get_articles('Technology')
+    technology_articles = get_articles('technology')
     title = 'News Highlights'
+
+    search_article = request.args.get('article_query')
+
+    if search_article:
+        return redirect(url_for('.search', search_word = search_article))
     return render_template('index.html', title = title, technology = technology_articles)
 
- 
+@main.route('/search/<search_word>')
+def search(search_word):
+    '''
+    Function to search for content
+    '''    
+    searched_articles = search_articles(search_word)
+    message = f'Search results for {search_word}'
+    return render_template('search.html', message = message, search = searched_articles)
--- app/requests.py
+++ app/requests.py
@@ -5,12 +5,14 @@ from .models import Source,Article
 api_key = None
 article_base_url = None
 source_base_url = None
+search_base_url = None
 
 def configure_request(app):
-    global api_key,article_base_url,source_base_url
+    global api_key,article_base_url,source_base_url,search_base_url
     api_key = app.config['NEWS_API_KEY']
     article_base_url = app.config['ARTICLE_BASE_URL']
     source_base_url = app.config['SOURCE_BASE_URL']
+    search_base_url = app.config['SEARCH_BASE_URL']
 
 def process_articles(article_dictionary):
     '''
@@ -25,12 +27,12 @@ def process_articles(article_dictionary):
         title = article.get('title')
         description = article.get('description')
         url = article.get('url')
-        category = article.get('category')
-        language = article.get('language')
-        country = article.get('country')
+        image_url = article.get('urlToImage')
+        date_published = article.get('publishedAt')
+        content = article.get('content')
 
-        if author:
-            article_object = Article(author,title,description,url,category,language,country)
+        if image_url:
+            article_object = Article(author,title,description,url,image_url,date_published,content)
             articles_list.append(article_object)
 
     return articles_list
@@ -41,6 +43,17 @@ def get_articles(query):
     Function to get articles based on the topic or keyword
     '''
     article_details = json.loads(urllib.request.urlopen(article_base_url.format(query,api_key)).read())
+
+    article_object = None
+    if article_details['articles']:
+        article_object = process_articles(article_details['articles'])
+    return article_object
+
+def search_articles(query):
+    '''
+    Function to get articles based on the topic or keyword
+    '''
+    article_details = json.loads(urllib.request.urlopen(search_base_url.format(query,api_key)).read())
 
     article_object = None
     if article_details['articles']:
